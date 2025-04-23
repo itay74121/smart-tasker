6397bc3c36197c950d698acb8a14c3c969c3f334
itay <itay74121@gmail.com>
2025-04-23 11:41:08 +0300

    made a function service

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @param {String} usernameOrEmail 
+ * @param {String} password 
+ */
+
+export function login(usernameOrEmail,password){
+    
+    
+    if(usernameOrEmail.includes('@')){ // its an email 
+        
+    }
+    else{ 
+
+    }
+
+}
+
+
