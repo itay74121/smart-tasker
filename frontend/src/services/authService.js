a9a94fe58efc9ebf1a1b2cdf322d667b4d761fc7
itay <itay74121@gmail.com>
2025-04-25 15:20:08 +0300

    remove hash version for now

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export async function login(username,password){
-    const hashversion = 1
+    // const hashversion = 1
     console.log(apiUrl)
     axios.post(`${apiUrl}\\api\\login`,{
         username:username,
