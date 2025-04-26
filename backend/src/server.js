f4d6108ce2966b1ce427608a7003d6425f62dec8
itay <itay74121@gmail.com>
2025-04-26 09:10:41 +0300

    cors again

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({origin:"*"}))
+app.use(cors({origin: ["http://localhost"]}))
 app.use(logger);
 app.use(cookieParser())
 app.use(express_jwt_middlewear)
